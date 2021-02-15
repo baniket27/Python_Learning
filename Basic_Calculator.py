c62e0046a08e8018f0a76e96502c009e9bcf4a6a
Aniket Anil Borse <71561560+baniket27@users.noreply.github.com>
2021-02-15 10:12:01 +0530

    Add files via upload

---
+++ Basic_Calculator.py
@@ -0,0 +1,8 @@
+from math import *
+
+print("Hello ! This is Basic calculator.")
+no1 = input("Enter any number: ")
+no2 = input("Enter any number: ")
+sum = float(no1) + float(no2)
+print(sum)
+print("Thank\nyou")
